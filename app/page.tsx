09696a09b8ed64e519a137bda2c4bfc9bbdb87a8
Aarav Jain <aaravjain272003@gmail.com>
2023-06-22 22:46:04 +0530

    started building a static demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,30 @@
-import Image from 'next/image'
-
-function ParallaxEffect() {
-	return <></>
-}
+import Image from "next/image"
 
 export default function Home() {
 	return (
 		<main>
-			<ParallaxEffect />
+			<p>
+				parallax effect here
+			</p>
+
+			<div>
+				<Image
+					src = "/../public/pic.jpg"
+					alt = "My photo ig"
+					width={250}
+					height={250}
+				/>
+
+				<p>
+					Aarav Jain
+				</p>
+				<p>
+					Software Developer
+				</p>
+				<p>
+					
+				</p>
+			</div>
 		</main>
 	)
 }
