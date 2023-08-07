bdb7fa42a2c28e8b316ee19e0d8c65893bb10d70
Aarav Jain <aaravjain272003@gmail.com>
2023-08-08 00:13:34 +0530

    fixed name and added quick links

--- app/page.tsx
+++ app/page.tsx
@@ -5,25 +5,38 @@ export default function Home() {
 		<main>
 			<p>
 				parallax effect here
-				//priority={false}
 			</p>
 
-			<div>
-				<Image
-					src = "/../public/pic.jpg"
-					alt = "My photo ig"
-					width = "720"
-					height = "900"
-				/>
-				<p>
-					Aarav Jain
-				</p>
-				<p>
-					Software Developer
-				</p>
-				<p>
-					A portfolio of my projects, and a project itself.
-				</p>
+			<div className = "flex m-40 flex-wrap justify-evenly justify-items-center">
+				<div className = "grid justify-items-center">
+					<Image
+						src = "/../public/pic.jpg"
+						alt = "My photo ig"
+						width = "200"
+						height = "250"
+					/>
+
+					<p className = "pt-8 text-5xl font-bold">
+						Aarav Jain
+					</p>
+
+					{/* <p className = "py-2 text-3xl font-thin">
+						Software Developer
+					</p> */}
+					
+					<ul className = "flex">
+						<li>Github</li>
+						<li>Instagram</li>
+						<li>Discord</li>
+						<li>Mail</li>
+					</ul>
+				</div>
+
+				<div>
+					<p>
+						A portfolio of my projects, and a project itself.
+					</p>
+				</div>
 			</div>
 		</main>
 	)
