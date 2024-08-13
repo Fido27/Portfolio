6b830b034be7d69a026190ec336197278bd17da3
Fido27 <aaravjain272003@gmail.com>
2024-08-14 05:23:35 +0530

    updated node modules

--- start_server.sh
+++ start_server.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 git pull
-python -m venv venv
-. venv/bin/activate
-pip install -r requirements.txt
-uvicorn app.api.main:app &
+# python -m venv venv
+# . venv/bin/activate
+# pip install -r requirements.txt
+# uvicorn app.api.main:app &
 # npm run dev
 npm run build
 npm run start
