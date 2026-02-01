0b82b06e1926e70507ee81d3b0fdf8a75510b71b
Aarav Jain <42429320+Fido27@users.noreply.github.com>
2026-02-01 13:56:09 -0600

    Refactor server startup script for better environment handling
    Updated virtual environment setup and server commands.

--- start_server.sh
+++ start_server.sh
@@ -3,11 +3,11 @@
 git pull
 
 ####### To directly run the servers #######
-# python3 -m venv venv
-# . venv/bin/activate
-# pip3 install -r requirements.txt
+python3 -m venv .venv
+. .venv/bin/activate
+pip install -r requirements.txt
 # python app/api/linuxmancyclopedia/ingest.py --device cpu
-# uvicorn app.api.main:app --reload &
-npm run dev
-# npm run build
-# npm run start
+uvicorn app.api.main:app &
+# npm run dev
+npm run build
+npm run start
